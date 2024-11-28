7be5a7280d4b644f02859fa3ed338487ba628717
bbrunopbb <bruno.almeida@ee.ufcg.edu.br>
2024-11-28 14:52:04 -0300

    update

--- IA/codigos/estudos/teste.py
+++ IA/codigos/estudos/teste.py
@@ -1,24 +1,61 @@
+import json
+
+import soundfile as sf
+import librosa
+import io
+import os
+import matplotlib.pyplot as plt
+
 import numpy as np
-import random
+import librosa.display
 
+LOCAL_DATA_FILE = 'local_data.json'
 
-training_data = [
-    (np.array([[0.1], [0.9]]), np.array([[1]])),
-    (np.array([[0.8], [0.2]]), np.array([[0]])),
-    (np.array([[0.4], [0.6]]), np.array([[1]])),
-    (np.array([[0.7], [0.3]]), np.array([[0]]))
-]
-
-mini_batche_size = 2
-epochs = 1
-n = len(training_data)
-training_data = list(training_data)
-
-for j in range(epochs):
-    random.shuffle(training_data)
-    mini_batches = [training_data[k:k+mini_batche_size] for k in range(0, n, mini_batche_size)]
+if not os.path.exists(LOCAL_DATA_FILE):
+    with open(LOCAL_DATA_FILE, 'w') as file:
+        json.dump({'audio': [], 'transcription': []}, file)
 
 
 
+data = {
+    'audio': [],
+    'transcription': []
+}
 
-print(mini_batches)
+
+
+samples1, sr1 = sf.read('/mnt/hd/audios_backup_test/VITRECTOMIA.wav')
+samples2, sr2 = sf.read('/mnt/hd/audios_backup_test/TROPONINA.wav')
+
+data['audio'].append({'array': samples1.tolist(), 'sampling_rate': sr1})
+data['transcription'].append('VITRECTOMIA')
+
+data['audio'].append({'array': samples2.tolist(), 'sampling_rate': sr2})
+data['transcription'].append('TROPONINA')
+
+with open(LOCAL_DATA_FILE, 'r+') as file:
+    local_data = json.load(file)
+    local_data['audio'].extend(data['audio'])
+    local_data['transcription'].extend(data['transcription'])
+    file.seek(0)
+    json.dump(local_data, file)
+    file.truncate()
+    
+audio, sr = librosa.load("/mnt/hd/audios_backup_test/VITRECTOMIA.wav", sr=16000)
+
+plt.figure(figsize=(10, 4))
+librosa.display.waveshow(audio, sr=sr)
+plt.title("Forma de Onda")
+plt.xlabel("Tempo (s)")
+plt.ylabel("Amplitude")
+plt.show()
+
+
+espectrograma = np.abs(librosa.stft(audio))
+
+# Exibir o espectrograma
+plt.figure(figsize=(10, 4))
+librosa.display.specshow(librosa.amplitude_to_db(espectrograma, ref=np.max), sr=sr, x_axis="time", y_axis="log")
+plt.title("Espectrograma")
+plt.colorbar(format="%+2.0f dB")
+plt.show()
